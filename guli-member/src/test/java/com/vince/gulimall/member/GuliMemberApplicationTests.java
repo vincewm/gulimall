2e23504528bd36ffeab9e72d7f6347467eb0ad5a
vince <2363659293@qq.com>
2022-09-21 00:09:59 +0800

    项目初始结构创建

---
+++ guli-member/src/test/java/com/vince/gulimall/member/GuliMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vince.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
